8b7ca1fb87a3a06f980c34ea3308324c6dae3ff2
David Silva <davidtvs10@gmail.com>
2018-04-08 16:42:12 +0100

    Implementation of 2D transposed convolution with output padding

---
+++ models/conv2d_transpose.py
@@ -0,0 +1,248 @@
+from keras.engine import InputSpec
+from keras.layers import Conv2D
+from keras.utils.conv_utils import normalize_tuple
+
+import keras.backend as K
+
+
+class Conv2DTranspose(Conv2D):
+    """Transposed convolution layer (sometimes called Deconvolution).
+    The need for transposed convolutions generally arises
+    from the desire to use a transformation going in the opposite direction
+    of a normal convolution, i.e., from something that has the shape of the
+    output of some convolution to something that has the shape of its input
+    while maintaining a connectivity pattern that is compatible with
+    said convolution.
+    When using this layer as the first layer in a model,
+    provide the keyword argument `input_shape`
+    (tuple of integers, does not include the sample axis),
+    e.g. `input_shape=(128, 128, 3)` for 128x128 RGB pictures
+    in `data_format="channels_last"`.
+    # Arguments
+        filters: Integer, the dimensionality of the output space
+            (i.e. the number of output filters in the convolution).
+        kernel_size: An integer or tuple/list of 2 integers, specifying the
+            width and height of the 2D convolution window.
+            Can be a single integer to specify the same value for
+            all spatial dimensions.
+        strides: An integer or tuple/list of 2 integers,
+            specifying the strides of the convolution along the width and height.
+            Can be a single integer to specify the same value for
+            all spatial dimensions.
+            Specifying any stride value != 1 is incompatible with specifying
+            any `dilation_rate` value != 1.
+        padding: one of `"valid"` or `"same"` (case-insensitive).
+        data_format: A string,
+            one of `channels_last` (default) or `channels_first`.
+            The ordering of the dimensions in the inputs.
+            `channels_last` corresponds to inputs with shape
+            `(batch, height, width, channels)` while `channels_first`
+            corresponds to inputs with shape
+            `(batch, channels, height, width)`.
+            It defaults to the `image_data_format` value found in your
+            Keras config file at `~/.keras/keras.json`.
+            If you never set it, then it will be "channels_last".
+        dilation_rate: an integer or tuple/list of 2 integers, specifying
+            the dilation rate to use for dilated convolution.
+            Can be a single integer to specify the same value for
+            all spatial dimensions.
+            Currently, specifying any `dilation_rate` value != 1 is
+            incompatible with specifying any stride value != 1.
+        activation: Activation function to use
+            (see [activations](../activations.md)).
+            If you don't specify anything, no activation is applied
+            (ie. "linear" activation: `a(x) = x`).
+        use_bias: Boolean, whether the layer uses a bias vector.
+        kernel_initializer: Initializer for the `kernel` weights matrix
+            (see [initializers](../initializers.md)).
+        bias_initializer: Initializer for the bias vector
+            (see [initializers](../initializers.md)).
+        kernel_regularizer: Regularizer function applied to
+            the `kernel` weights matrix
+            (see [regularizer](../regularizers.md)).
+        bias_regularizer: Regularizer function applied to the bias vector
+            (see [regularizer](../regularizers.md)).
+        activity_regularizer: Regularizer function applied to
+            the output of the layer (its "activation").
+            (see [regularizer](../regularizers.md)).
+        kernel_constraint: Constraint function applied to the kernel matrix
+            (see [constraints](../constraints.md)).
+        bias_constraint: Constraint function applied to the bias vector
+            (see [constraints](../constraints.md)).
+    # Input shape
+        4D tensor with shape:
+        `(batch, channels, rows, cols)` if data_format='channels_first'
+        or 4D tensor with shape:
+        `(batch, rows, cols, channels)` if data_format='channels_last'.
+    # Output shape
+        4D tensor with shape:
+        `(batch, filters, new_rows, new_cols)` if data_format='channels_first'
+        or 4D tensor with shape:
+        `(batch, new_rows, new_cols, filters)` if data_format='channels_last'.
+        `rows` and `cols` values might have changed due to padding.
+    # References
+        - [A guide to convolution arithmetic for deep learning](https://arxiv.org/abs/1603.07285v1)
+        - [Deconvolutional Networks](http://www.matthewzeiler.com/pubs/cvpr2010/cvpr2010.pdf)
+    """
+
+    def __init__(
+        self,
+        filters,
+        kernel_size,
+        strides=(1, 1),
+        padding='valid',
+        output_padding=0,
+        data_format=None,
+        activation=None,
+        use_bias=True,
+        kernel_initializer='glorot_uniform',
+        bias_initializer='zeros',
+        kernel_regularizer=None,
+        bias_regularizer=None,
+        activity_regularizer=None,
+        kernel_constraint=None,
+        bias_constraint=None,
+        **kwargs
+    ):
+        super(Conv2DTranspose, self).__init__(
+            filters,
+            kernel_size,
+            strides=strides,
+            padding=padding,
+            data_format=data_format,
+            activation=activation,
+            use_bias=use_bias,
+            kernel_initializer=kernel_initializer,
+            bias_initializer=bias_initializer,
+            kernel_regularizer=kernel_regularizer,
+            bias_regularizer=bias_regularizer,
+            activity_regularizer=activity_regularizer,
+            kernel_constraint=kernel_constraint,
+            bias_constraint=bias_constraint,
+            **kwargs
+        )
+        self.input_spec = InputSpec(ndim=4)
+        self.output_padding = normalize_tuple(
+            output_padding, 2, 'output_padding'
+        )
+
+    def build(self, input_shape):
+        if len(input_shape) != 4:
+            raise ValueError(
+                'Inputs should have rank ' + str(4) +
+                '; Received input shape:', str(input_shape)
+            )
+        if self.data_format == 'channels_first':
+            channel_axis = 1
+        else:
+            channel_axis = -1
+        if input_shape[channel_axis] is None:
+            raise ValueError(
+                'The channel dimension of the inputs '
+                'should be defined. Found `None`.'
+            )
+        input_dim = input_shape[channel_axis]
+        kernel_shape = self.kernel_size + (self.filters, input_dim)
+
+        self.kernel = self.add_weight(
+            shape=kernel_shape,
+            initializer=self.kernel_initializer,
+            name='kernel',
+            regularizer=self.kernel_regularizer,
+            constraint=self.kernel_constraint
+        )
+        if self.use_bias:
+            self.bias = self.add_weight(
+                shape=(self.filters, ),
+                initializer=self.bias_initializer,
+                name='bias',
+                regularizer=self.bias_regularizer,
+                constraint=self.bias_constraint
+            )
+        else:
+            self.bias = None
+        # Set input spec.
+        self.input_spec = InputSpec(ndim=4, axes={channel_axis: input_dim})
+        self.built = True
+
+    def call(self, inputs):
+        input_shape = K.shape(inputs)
+        batch_size = input_shape[0]
+        if self.data_format == 'channels_first':
+            h_axis, w_axis = 2, 3
+        else:
+            h_axis, w_axis = 1, 2
+
+        height, width = input_shape[h_axis], input_shape[w_axis]
+        kernel_h, kernel_w = self.kernel_size
+        stride_h, stride_w = self.strides
+        out_pad_h, out_pad_w = self.output_padding
+
+        # Infer the dynamic output shape:
+        out_height = deconv_length(
+            height, stride_h, kernel_h, self.padding, out_pad_h
+        )
+        out_width = deconv_length(
+            width, stride_w, kernel_w, self.padding, out_pad_w
+        )
+        if self.data_format == 'channels_first':
+            output_shape = (batch_size, self.filters, out_height, out_width)
+        else:
+            output_shape = (batch_size, out_height, out_width, self.filters)
+
+        outputs = K.conv2d_transpose(
+            inputs,
+            self.kernel,
+            output_shape,
+            self.strides,
+            padding=self.padding,
+            data_format=self.data_format
+        )
+
+        if self.bias:
+            outputs = K.bias_add(
+                outputs, self.bias, data_format=self.data_format
+            )
+
+        if self.activation is not None:
+            return self.activation(outputs)
+        return outputs
+
+    def compute_output_shape(self, input_shape):
+        output_shape = list(input_shape)
+        if self.data_format == 'channels_first':
+            c_axis, h_axis, w_axis = 1, 2, 3
+        else:
+            c_axis, h_axis, w_axis = 3, 1, 2
+
+        kernel_h, kernel_w = self.kernel_size
+        stride_h, stride_w = self.strides
+        out_pad_h, out_pad_w = self.output_padding
+
+        output_shape[c_axis] = self.filters
+        output_shape[h_axis] = deconv_length(
+            output_shape[h_axis], stride_h, kernel_h, self.padding, out_pad_h
+        )
+        output_shape[w_axis] = deconv_length(
+            output_shape[w_axis], stride_w, kernel_w, self.padding, out_pad_w
+        )
+        return tuple(output_shape)
+
+    def get_config(self):
+        config = super(Conv2DTranspose, self).get_config()
+        config.pop('dilation_rate')
+        return config
+
+
+def deconv_length(dim_size, stride_size, kernel_size, padding, output_padding):
+    assert padding in {'same', 'valid', 'full'}
+    if padding == 'same':
+        pad = kernel_size // 2
+    elif padding == 'valid':
+        pad = 0
+    elif padding == 'full':
+        pad = kernel_size - 1
+
+    output_dim = (dim_size - 1) * stride_size + kernel_size - 2 * pad + output_padding  # yapf: disable
+
+    return output_dim
