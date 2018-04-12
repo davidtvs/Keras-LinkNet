db0089c83e45b8bbaa87b58ddaefedcbc07d6e59
David Silva <davidtvs10@gmail.com>
2018-04-13 00:42:29 +0100

    Add arguments to main.py

---
+++ args.py
@@ -0,0 +1,110 @@
+from argparse import ArgumentParser
+
+
+def get_arguments():
+    """Defines command-line arguments, and parses them."""
+    parser = ArgumentParser()
+
+    # Execution mode
+    parser.add_argument(
+        "--mode",
+        "-m",
+        choices=['train'],
+        default='train',
+        help=(
+            "train: performs training and validation; test: tests the model "
+            "found in \"--save_dir\" with name \"--name\" on \"--dataset\"; "
+            "full: combines train and test modes. Default: train"
+        )
+    )
+
+    # Hyperparameters
+    parser.add_argument(
+        "--batch_size",
+        "-b",
+        type=int,
+        default=10,
+        help="The batch size. Default: 10"
+    )
+    parser.add_argument(
+        "--epochs",
+        type=int,
+        default=300,
+        help="Number of training epochs. Default: 300"
+    )
+    parser.add_argument(
+        "--learning_rate",
+        "-lr",
+        type=float,
+        default=5e-4,
+        help="The learning rate. Default: 5e-4"
+    )
+    parser.add_argument(
+        "--lr_decay",
+        type=float,
+        default=0.1,
+        help="The learning rate decay factor. Default: 0.5"
+    )
+    parser.add_argument(
+        "--lr_decay_epochs",
+        type=int,
+        default=100,
+        help=(
+            "The number of epochs before adjusting the learning rate. "
+            "Default: 100"
+        )
+    )
+
+    # Dataset
+    parser.add_argument(
+        "--dataset",
+        choices=['camvid'],
+        default='camvid',
+        help="Dataset to use. Default: camvid"
+    )
+    parser.add_argument(
+        "--dataset_dir",
+        type=str,
+        default="data/CamVid",
+        help=(
+            "Path to the root directory of the selected dataset. "
+            "Default: data/CamVid"
+        )
+    )
+    parser.add_argument(
+        "--weighing",
+        choices=['enet', 'mfb', 'none'],
+        default='ENet',
+        help=(
+            "The class weighing technique to apply to the dataset. "
+            "Default: enet"
+        )
+    )
+    parser.add_argument(
+        "--ignore_unlabelled",
+        type=bool,
+        default=True,
+        help=(
+            "If True, the unlabelled class weight is ignored (set to 0); "
+            "otherwise, it's kept as computed. Default: True"
+        )
+    )
+
+    # Settings
+    parser.add_argument(
+        "--workers",
+        type=int,
+        default=4,
+        help="Number of subprocesses to use for data loading. Default: 4"
+    )
+    parser.add_argument(
+        "--verbose",
+        choices=[0, 1, 2],
+        default=1,
+        help=(
+            "Verbosity mode: 0 - silent, 1 - progress bar, 2 - one line per "
+            "epoch. Default: 1"
+        )
+    )
+
+    return parser.parse_args()
--- main.py
+++ main.py
@@ -1,16 +1,84 @@
-from data import CamVidGenerator as DataGenerator
 from models.linknet import LinkNet
+from keras import metrics
+from keras.optimizers import Adam
+from keras.callbacks import LearningRateScheduler
+
+from args import get_arguments
+from data.utils import enet_weighing, median_freq_balancing
 
 if __name__ == '__main__':
-    train_generator = DataGenerator(
-        "/home/davidtvs/Deep-Learning/Datasets/CamVid", batch_size=10
-    )
-    print(len(train_generator))
-    image_batch, label_batch = train_generator[0]
-    print(image_batch.shape)
-    print(label_batch.shape)
+    args = get_arguments()
 
-    model = LinkNet(
-        label_batch.shape[-1], input_shape=image_batch[0].shape
-    ).get_model()
+    # Import the requested dataset
+    if args.dataset.lower() == 'camvid':
+        from data import CamVidGenerator as DataGenerator
+    else:
+        # Should never happen...but just in case it does
+        raise RuntimeError(
+            "\"{0}\" is not a supported dataset.".format(args.dataset)
+        )
+
+    # Initialize the dataset generator
+    train_generator = DataGenerator(
+        args.dataset_dir, batch_size=args.batch_size
+    )
+
+    # Some information about the dataset
+    image_batch, label_batch = train_generator[0]
+    num_classes = label_batch[0].shape[-1]
+    print("Training dataset size: {}".format(len(train_generator)))
+    print("Image size {}".format(image_batch.shape))
+    print("Label size {}".format(label_batch.shape))
+    print("Number of classes (including unlabelled) {}".format(num_classes))
+
+    # Compute class weights if needed
+    print("Weighing technique: {}".format(args.weighing))
+    if (args.weighing is not None):
+        print("Computing class weights...")
+        print("(this can take a while depending on the dataset size)")
+        if args.weighing.lower() == 'enet':
+            class_weights = enet_weighing(train_generator, num_classes)
+        elif args.weighing.lower() == 'mfb':
+            class_weights = median_freq_balancing(train_generator, num_classes)
+        else:
+            class_weights = None
+    else:
+        class_weights = None
+
+    # Set the unlabelled class weight to 0 if requested
+    if class_weights is not None:
+        # Handle unlabelled class
+        if args.ignore_unlabelled:
+            if args.dataset.lower() == 'camvid':
+                class_weights[-1] = 0
+
+    print("Class weights: {}".format(class_weights))
+
+    # Create the model
+    model = LinkNet(num_classes, input_shape=image_batch[0].shape).get_model()
     print(model.summary())
+
+    # Compile the model
+    # Optimizer: Adam
+    optim = Adam(args.learning_rate)
+    # Loss: Categorical crossentropy loss
+    model.compile(
+        optimizer=optim,
+        loss='categorical_crossentropy',
+        metrics=[metrics.categorical_accuracy]
+    )
+
+    # Set up learining rate scheduler
+    def _lr_decay(epoch, learning_rate):
+        return args.lr_decay**(epoch // args.lr_decay_epochs) * args.learning_rate
+    lr_scheduler = LearningRateScheduler(_lr_decay)
+
+    # Train the model
+    model.fit_generator(
+        train_generator,
+        class_weight=class_weights,
+        epochs=args.epochs,
+        callbacks=[lr_scheduler],
+        workers=args.workers,
+        verbose=args.verbose
+    )
